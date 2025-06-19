2bb6faafa077bda833694a383651248b26aee67d
DryHop2 <tyler.savon.smith@gmail.com>
2025-06-19 07:48:00 -0600

    Add nocase modifier support to content rules

--- snort_rulegen/rule_templates.py
+++ snort_rulegen/rule_templates.py
@@ -1,5 +1,15 @@
-def build_rule(proto, src_ip, src_port, dst_ip, dst_port, msg, content, sid, rev=1):
-    return(
-        f'alert {proto} {src_ip} {src_port} -> {dst_ip} {dst_port} '
-        f'(msg:"{msg}"; content:"{content}"; sid:{sid}; rev:{rev};)'
-    )
+def build_rule(proto, src_ip, src_port, dst_ip, dst_port, msg, content, sid, rev=1, nocase=False):
+    options = [
+        f'msg:"{msg}"',
+    ]
+
+    if content:
+        options.append(f'content:"{content}"')
+        if nocase:
+            options.append("nocase")
+
+    options.append(f"sid:{sid}")
+    options.append(f"rev:{rev}")
+
+    rule = f'alert {proto} {src_ip} {src_port} -> {dst_ip} {dst_port} ({"; ".join(options)};)'
+    return rule
